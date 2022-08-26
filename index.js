1c7497763d27d2fe134ecfe0319424a962a56487
Randima Dias <randimamethminid525@gmail.com>
2022-08-26 23:46:16 +0530

    added /info route

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.post("/info", (req, res) => {
+  const body = req.body;
+  res.send(`Your name is ${body.name} and you were born in ${body.birthday}`);
+});
+
+app.listen(3000, () => {
+  console.log("Server started listening on port 3000\nhttp://localhost:3000");
+});
